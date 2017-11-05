b05e5c3fbba0ae02259f0355b927ad6256c6ab3c
gogolearn <jay.jongcheol.park@gmail.com>
2017-11-05 09:25:48 -0500

    Add Home Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,14 @@ class Header extends Component {
   render () {
     return (
       <div className="mb-5">
-        {this.props.authenticated ?
+        { this.props.authenticated ?
           <Link className="btn btn-outline-primary float-right" to="/logout">Logout</Link>
           : <Link className="btn btn-outline-primary float-right" to="/login">Login</Link>
         }
-        <Link className="btn btn-outline-dark float-left mr-5" to="/dashboard">Dashboard</Link>
+          <div>
+            <Link className="btn btn-outline-dark float-left mr-3" to="/">Home</Link>
+            <Link className="btn btn-outline-dark float-left" to="/dashboard">Dashboard</Link>
+          </div>
         <div className="clearfix"></div>
       </div>
 
