83a1da4682eae90ef9cb8902b56effa097716c87
gogolearn <jay.jongcheol.park@gmail.com>
2017-11-05 01:44:56 -0400

    Header bottom margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 class Header extends Component {
   render () {
     return (
-      <div className="mb-3">
+      <div className="mb-5">
         {this.props.authenticated ?
           <Link className="btn btn-outline-primary float-right" to="/logout">Logout</Link>
           : <Link className="btn btn-outline-primary float-right" to="/login">Login</Link>
